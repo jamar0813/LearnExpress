5e5bdd3549efa3d962f518fa8ff57ccb3637e257
Jamar <jamar@publicbloc.io>
2020-11-16 01:05:52 +0100

    first commit

---
+++ Members.js
@@ -0,0 +1,24 @@
+const members = [
+    {
+        id: 1,
+        name: "Tina Rey",
+        email: "tina@mail.com",
+        status: "active"
+    },
+
+    {
+        id: 2,
+        name: "PJ",
+        email: "pj@mail.com",
+        status: "inactive"
+    },
+
+    {
+        id: 3,
+        name: "Gman",
+        email: "gman@mail.com",
+        status: "active"
+    },
+]
+
+module.exports = members;
